05da65ea9f28cae2cc11cf0471eac7666fdbe949
pancho646 <pancho646@gmail.com>
2022-04-13 16:06:51 -0400

    13-4

--- app/Http/Controllers/DatosController.php
+++ app/Http/Controllers/DatosController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Datos;
+use App\Http\Requests\DatosRequest;
 class DatosController extends Controller
 {
     //
     public function create(){
         return view('consulta.datos');
+    }
+    public function store(DatosRequest $request){
+        $datos = new Datos();
+
+        $datos->firstname = $request->firstname;
+        $datos->secondname = $request->secondname;
+        $datos->lastname1 = $request->lastname1;
+        $datos->lastname2 = $request->lastname2;
+        $datos->cedula = $request->cedula;
+        $datos->historia = $request->historia;
+        $datos->nacionalidad = $request->nacionalidad;
+        $datos->municipio = $request->municipio;
+        $datos->fecha_nac = $request->fecha_nac;
+        $datos->ciudad = $request->ciudad;
+        $datos->estado = $request->estado;
+        $datos->parroquia = $request->parroquia;
+        $datos->sexo = $request->sexo;
+        $datos->direccion = $request->direccion;
+        $datos->telf_hab = $request->telf_hab;
+        $datos->telf_cel = $request->telf_cel;
+        $datos->email= $request->email;
+        $datos->nivel_ins = $request->nivel_ins;
+        $datos->prof_of = $request->prof_of;
+        
+
     }
 }
--- app/Http/Controllers/MedicamentoController.php
+++ app/Http/Controllers/MedicamentoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\medicamento;
 class MedicamentoController extends Controller
 {
     //
@@ -12,7 +12,12 @@ class MedicamentoController extends Controller
     }
 
     public function store(Request $request){
-            
+        $medicamento = new Medicamento(); 
+        $medicamento->codigomed = $request->codigomed;
+        $medicamento->nombre = $request->nombre;
+        $medicamento->descripcion = $request->descripcion;
+        $medicamento->save();
+        return redirect()->route('smed');
 
     }
 }
